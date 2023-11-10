7eb35b66b67e7aa862b4d766eeb29cf7c890b775
Trying-1 bait <tryingbait@gmail.com>
2023-11-10 12:54:04 +0000

    second server started

---
+++ secondserver/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+const app=express()
+app.use(express.json())
+app.listen(3000,()=>console.log("listening to port 3000..."))
+app.get("/firstRoute",(req,res)=>{
+    res.send("hello world")
+})
+app.post("/firstRoute",(req,res)=>{
+    const {name,email,password}=req.body;
+    const data={
+        "name":name,
+        "email":email,
+        "password":password
+    }
+    res.send(data)
+})
